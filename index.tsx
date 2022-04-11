46eec0d895f259e7b6ccfa778eb3b4d2db7da67e
foxbrio <im.foxbrio@gmail.com>
2022-04-11 20:51:25 +0530

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+
+interface AppProps {}
+interface AppState {
+  name: string;
+}
+
+class App extends Component<AppProps, AppState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'React',
+    };
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+          <Hello name={this.state.name} />
+          <p>Start editing to see some magic happen :)</p>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
